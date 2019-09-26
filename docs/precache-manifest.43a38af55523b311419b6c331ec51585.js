27960f52897fa5cf5fed061ce7ca6ea78080851e
Marco A. Rojas <marco.rojas@alumno.buap.mx>
2019-09-26 17:52:48 -0400

    Dropdop static

---
+++ docs/precache-manifest.43a38af55523b311419b6c331ec51585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee2fcc4ffacfc843b3997668f8a8af3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13604f043ce29915cf46",
+    "url": "/static/css/main.e3ca132d.chunk.css"
+  },
+  {
+    "revision": "829a5e37590ee21c21d0",
+    "url": "/static/js/2.3de245d5.chunk.js"
+  },
+  {
+    "revision": "13604f043ce29915cf46",
+    "url": "/static/js/main.1eb7644c.chunk.js"
+  },
+  {
+    "revision": "18f09eb9d20de40248bf",
+    "url": "/static/js/runtime-main.c53ee908.js"
+  }
+]);
